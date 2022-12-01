592f6f8e9917929effac7beaf61c79b46eca36e2
SREYPHEAP.KHIN <sreypheap.khin@student.passerellesnumeriques.org>
2022-12-01 08:25:23 +0700

    [MENU BAR] : page menu

--- views/js/main.js
+++ views/js/main.js
@@ -1,6 +1,7 @@
-function createCard(title, price, qty, col) {
+function createCard(title, price, qty, col, img){
     const card = document.createElement("div");
     card.classList.add("card")
+    
     const titleElement = document.createElement("div");
     titleElement.classList.add("title");
     titleElement.textContent = title;
@@ -20,9 +21,14 @@ function createCard(title, price, qty, col) {
     colElement.classList.add("col");
     colElement.textContent = col;
 
+    const imgElement = document.createElement("img");
+    // imgElement.classList.add("img");
+    imgElement.src = img;
+
     cardBody.appendChild(priceElement);
     cardBody.appendChild(qtyElement);
     cardBody.appendChild(colElement);
+    cardBody.appendChild(imgElement);
 
     card.appendChild(titleElement);
     card.appendChild(cardBody);
@@ -31,12 +37,29 @@ function createCard(title, price, qty, col) {
 }
 
 function displayProduct() {
-    let products = JSON.parse(localStorage.getItem('product-name')) ?? [];
+    let products = JSON.parse(localStorage.getItem("product-name")) ?? [];
     for (let product of products) {
-        let card = createCard(product.name, product.price, product.quantity, product.color);
+        let card = createCard(product.name, product.price, product.quantity, product.color, product.Img);
         container.appendChild(card);
     }
 }
 
-const container = document.querySelector('#container');
-document.addEventListener('DOMContentLoaded', () => { displayProduct(); });
+const container = document.querySelector("#container");
+document.addEventListener("DOMContentLoaded", () => { displayProduct() });
+
+// function searchProducts(event){
+//     const getProduct = searchProductsInput.value.toLowerCase();
+//     const getProductlist = document.querySelector(".title");
+//     for(let i in getProductlist){
+//         getProductlist[i] .parentNode.style.display = "none";
+//         if(getProductlist[i].textContent.toLowerCase() === getProduct || getProductlist[i].textContent.toLowerCase().includes(getProduct)){
+//             getProductlist[i].parentNode.style.display = "block"
+//         }
+//     }
+
+
+// }
+// let searchProductsInput = document
+//     .getElementById("search-products")
+//     .querySelector("input")
+// searchProductsInput.addEventListener("keyup", s)
--- views/seller/js/seller.js
+++ views/seller/js/seller.js
@@ -6,21 +6,24 @@ function getProductFromLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key)) ?? [];
 }
 
-function createNewRecord(name, price, quantity, color) {
+function createNewRecord(name, price, quantity, color, img) {
     const tr = document.createElement("tr");
     const tdOne = document.createElement("td");
     const tdTwo = document.createElement("td");
     const tdThree = document.createElement("td");
     const tdFour = document.createElement("td");
+    const tdFive = document.createElement("td");
     tdOne.textContent = name;
     tdTwo.textContent = price;
     tdThree.textContent = quantity;
     tdFour.textContent = color;
+    tdFive.textContent = img;
 
     tr.appendChild(tdOne);
     tr.appendChild(tdTwo);
     tr.appendChild(tdThree);
     tr.appendChild(tdFour);
+    tr.appendChild(tdFive);
 
     return tr;
 
@@ -32,15 +35,18 @@ function createTableHeader() {
     const thTwo = document.createElement("th");
     const thThree = document.createElement("th");
     const thFour = document.createElement("th")
+    const thFive = document.createElement("th")
     thOne.textContent = "name";
     thTwo.textContent = "price";
     thThree.textContent = "quantity";
     thFour.textContent = "color"
+    thFive.textContent = "img"
 
     headerRow.appendChild(thOne);
     headerRow.appendChild(thTwo);
     headerRow.appendChild(thThree);
     headerRow.appendChild(thFour);
+    headerRow.appendChild(thFive);
 
     return headerRow;
 }
@@ -53,7 +59,7 @@ function displayProduct() {
     newTable.appendChild(createTableHeader());
     let products = getProductFromLocalStorage("product-name");
     for (let product of products) {
-        let row = createNewRecord(product.name, product.price, product.quantity, product.color);
+        let row = createNewRecord(product.name, product.price, product.quantity, product.color, product.Img);
         newTable.appendChild(row)
     }
     tableData.appendChild(newTable);
@@ -65,6 +71,7 @@ const productName = document.querySelector("#product-name");
 const productPrice = document.querySelector("#product-price");
 const productQuantity = document.querySelector("#product-quantity");
 const productColor = document.querySelector("#product-color");
+const productImg = document.querySelector("#product-img");
 const button = document.querySelector("button");
 const tableData = document.querySelector(".table-data");
 
@@ -75,7 +82,7 @@ button.addEventListener("click", (e) => {
     if (productName.value === "") {
         return;
     }
-    let productObject = {name: productName.value, price: productPrice.value, quantity: productQuantity.value, color: productColor.value}
+    let productObject = {name: productName.value, price: productPrice.value, quantity: productQuantity.value, color: productColor.value, Img: productImg.value}
 
     productList.push(productObject);
 
@@ -83,6 +90,7 @@ button.addEventListener("click", (e) => {
     productPrice.value = ""
     productQuantity.value = ""
     productColor.value = ""
+    productImg.value = ""
     // add the product
     addProductToLocalStorage("product-name", JSON.stringify(productList));
 
@@ -91,4 +99,4 @@ button.addEventListener("click", (e) => {
 
 
 
-document.addEventListener('DOMContentLoaded', () => { displayProduct() })
+document.addEventListener("DOMContentLoaded", () => { displayProduct() })
